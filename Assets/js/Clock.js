ac0a07887f04279df10a36f45af487e61b3ed95a
Lucas <lukinha_araraquara@hotmail.com>
2018-09-20 12:35:43 -0400

    Changed the pattern

--- Assets/js/Clock.js
+++ Assets/js/Clock.js
@@ -1,24 +1,24 @@
 function Clock(ctx, microwave){
-	this.x = microwave.x + (microwave.w * 31/45)
-	this.y = microwave.y + (microwave.h * 1/4)
-	this.fontSize = microwave.w * 1/18
+  this.x = microwave.x + (microwave.w * 31/45);
+  this.y = microwave.y + (microwave.h * 1/4);
+  this.fontSize = microwave.w * 1/18;
 
-	this.timer = function(){
-        ctx.save()
-        ctx.font = `${this.fontSize}px Arial`
-        ctx.fillText(this.getHour(), this.x, this.y)
-        ctx.restore()
-    }
+  this.timer = function(){
+    ctx.save();
+    ctx.font = `${this.fontSize}px Arial`;
+    ctx.fillText(this.getHour(), this.x, this.y);
+    ctx.restore();
+  }
 
-    this.getHour = function(){
-        var date = new Date()
-        var hours = this.formatTime(date.getHours())
-        var minutes = this.formatTime(date.getMinutes())
-        var seconds = this.formatTime(date.getSeconds())
-        return `${hours}:${minutes}:${seconds}`
-    }
+  this.getHour = function(){
+    let date = new Date();
+    let hours = this.formatTime(date.getHours());
+    let minutes = this.formatTime(date.getMinutes());
+    let seconds = this.formatTime(date.getSeconds());
+    return `${hours}:${minutes}:${seconds}`;
+  }
 
-    this.formatTime = function(date){
-        return date < 10 ? `0${date}` : date
-    }
+  this.formatTime = function(date){
+    return date < 10 ? `0${date}` : date;
+  }
 }
--- Assets/js/js.js
+++ Assets/js/js.js
@@ -1,35 +1,35 @@
 window.onload = function(){
-    var guest = document.getElementById('microwave')
-    var cnv = document.querySelector('canvas')
-    var ctx = cnv.getContext('2d')
+  let guest = document.getElementById('microwave');
+  let cnv = document.querySelector('canvas');
+  let ctx = cnv.getContext('2d');
 
-    var microwave = new Microwave(ctx)
-    microwave.clock = new Clock(ctx, microwave)
+  let microwave = new Microwave(ctx);
+  microwave.clock = new Clock(ctx, microwave);
 
-    function cleanScreen(){
-        ctx.save()
-        ctx.fillStyle = '#fff'
-        ctx.clearRect(0, 0, cnv.width, cnv.height)
-        ctx.restore()
-    }
+  function cleanScreen(){
+    ctx.save();
+    ctx.fillStyle = '#fff';
+    ctx.clearRect(0, 0, cnv.width, cnv.height);
+    ctx.restore();
+  }
 
-    function setGuestName(){
-        var name = prompt('What\'s your name:')
-        validateGuestName(name)
-    }
+  function setGuestName(){
+    let name = prompt(`What's your name:`);
+    validateGuestName(name);
+  }
 
-    function validateGuestName(name){
-        var condition = name.trim() !== '' && name.length < 20
-        guest.textContent = condition ? `Microwave of ${name}` : 'Microwave'
-    }
+  function validateGuestName(name){
+    let condition = name.trim() !== '' && name.length < 20;
+      guest.textContent = condition ? `Microwave of ${name}` : 'Microwave';
+  }
 
-    function loop(){
-        window.requestAnimationFrame(loop, cnv)
-        cleanScreen()
-        microwave.draw()
-        microwave.clock.timer()
-    }
+  function loop(){
+    window.requestAnimationFrame(loop, cnv);
+    cleanScreen();
+    microwave.draw();
+    microwave.clock.timer();
+  }
 
-    setGuestName()
-    loop()
+  setGuestName();
+  loop();
 }
